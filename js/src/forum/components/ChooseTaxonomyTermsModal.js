4459df54b333184065e913eeb5a830e7d8145e22
Clark Winkelmann <clark.winkelmann@gmail.com>
2020-12-29 22:44:20 +0100

    Improve extensibility of the taxonomy modal

--- js/src/forum/components/ChooseTaxonomyTermsModal.js
+++ js/src/forum/components/ChooseTaxonomyTermsModal.js
@@ -5,6 +5,7 @@ import Button from 'flarum/components/Button';
 import LoadingIndicator from 'flarum/components/LoadingIndicator';
 import highlight from 'flarum/helpers/highlight';
 import classList from 'flarum/utils/classList';
+import ItemList from 'flarum/utils/ItemList';
 import extractText from 'flarum/utils/extractText';
 import KeyboardNavigatable from 'flarum/utils/KeyboardNavigatable';
 
@@ -49,6 +50,7 @@ export default class ChooseTaxonomyTermsModal extends Modal {
         this.searchFilter = '';
         this.activeListIndex = 0;
         this.inputIsFocused = false;
+        this.saving = false;
 
         if (this.props.selectedTerms) {
             this.props.selectedTerms.forEach(this.addTerm.bind(this));
@@ -74,7 +76,9 @@ export default class ChooseTaxonomyTermsModal extends Modal {
             .onUp(() => this.setIndex(this.activeListIndex - 1, true))
             .onDown(() => this.setIndex(this.activeListIndex + 1, true))
             .onSelect(this.select.bind(this))
-            .onRemove(() => this.selectedTerms.splice(this.selectedTerms.length - 1, 1));
+            .onRemove(() => {
+                this.toggleTerm(this.selectedTerms[this.selectedTerms.length - 1]);
+            });
     }
 
     indexInSelectedTerms(term) {
@@ -114,12 +118,14 @@ export default class ChooseTaxonomyTermsModal extends Modal {
         if (this.props.taxonomy.minTerms() && count < this.props.taxonomy.minTerms()) {
             const remaining = this.props.taxonomy.minTerms() - count;
             return app.translator.transChoice('fof-taxonomies.forum.modal.placeholder', remaining, {remaining});
+        } else if (count === 0) {
+            return app.translator.trans('fof-taxonomies.forum.modal.placeholderOptional');
         }
 
         return '';
     }
 
-    content() {
+    filteredAvailableTerms() {
         let availableTerms = this.availableTerms === null ? [] : this.availableTerms;
         const filter = this.searchFilter.toLowerCase();
 
@@ -158,63 +164,106 @@ export default class ChooseTaxonomyTermsModal extends Modal {
             availableTerms = [];
         }
 
+        return availableTerms;
+    }
+
+    content() {
+        return [
+            this.viewForm(),
+            this.listAvailableTerms(this.filteredAvailableTerms()),
+        ];
+    }
+
+    viewForm() {
         const description = this.props.taxonomy.description();
 
-        return [
-            m('.Modal-body', [
-                description ? m('p', description) : null,
-                m('.ChooseTaxonomyTermsModal-form', [
-                    m('.ChooseTaxonomyTermsModal-form-input', m('.TermsInput.FormControl', {
-                        className: this.inputIsFocused ? 'focus' : '',
-                    }, [
-                        m('span.TermsInput-selected', this.selectedTerms.map(term => {
-                            return m('span.TermsInput-term', {
-                                onclick: () => {
-                                    this.removeTerm(term);
-                                    this.onready();
-                                },
-                            }, termLabel(term));
-                        })),
-                        m('input.FormControl', {
-                            placeholder: extractText(this.getInstruction()),
-                            value: this.searchFilter,
-                            oninput: event => {
-                                this.searchFilter = event.target.value;
-                                this.activeListIndex = 0;
-                            },
-                            onkeydown: this.navigator.navigate.bind(this.navigator),
-                            onfocus: () => this.inputIsFocused = true,
-                            onblur: () => this.inputIsFocused = false,
-                        }),
-                    ])),
-                    m('.ChooseTaxonomyTermsModal-form-submit.App-primaryControl', Button.component({
+        return m('.Modal-body', [
+            description ? m('p', description) : null,
+            m('.ChooseTaxonomyTermsModal-form', this.formItems().toArray()),
+        ]);
+    }
 
-                        type: 'submit',
-                        className: 'Button Button--primary',
-                        disabled: this.props.taxonomy.minTerms() && this.selectedTerms.length < this.props.taxonomy.minTerms(),
-                        icon: 'fas fa-check',
-                    }, app.translator.trans('fof-taxonomies.forum.modal.submit'))),
-                ]),
-            ]),
-            m('.Modal-footer', this.availableTerms === null ? LoadingIndicator.component() : m('ul.ChooseTaxonomyTermsModal-list.SelectTermList', availableTerms
-                .map((term, index) => m('li', {
-                    'data-index': index,
-                    className: classList({
-                        colored: !!term.color(),
-                        selected: this.indexInSelectedTerms(term) !== -1,
-                        active: this.activeListIndex === index,
-                    }),
-                    style: {color: term.color()},
-                    onmouseover: () => this.activeListIndex = index,
-                    onclick: this.toggleTerm.bind(this, term),
-                }, [
-                    taxonomyIcon(term),
-                    m('span.SelectTermListItem-name', term.exists ? highlight(term.name(), filter) : app.translator.trans('fof-taxonomies.forum.modal.custom', {
-                        value: m('em', term.name()),
-                    })),
-                    term.description() ? m('span.SelectTermListItem-description', term.description()) : '',
-                ])))),
-        ];
+    formItems() {
+        const items = new ItemList();
+
+        items.add('input', m('.ChooseTaxonomyTermsModal-form-input', m('.TermsInput.FormControl', {
+            className: this.inputIsFocused ? 'focus' : '',
+        }, this.inputItems().toArray())), 20);
+
+        items.add('submit', m('.ChooseTaxonomyTermsModal-form-submit.App-primaryControl', Button.component({
+            type: 'submit',
+            className: 'Button Button--primary',
+            disabled: this.props.taxonomy.minTerms() && this.selectedTerms.length < this.props.taxonomy.minTerms(),
+            icon: 'fas fa-check',
+            loading: this.saving,
+        }, app.translator.trans('fof-taxonomies.forum.modal.submit'))), 10);
+
+        return items;
+    }
+
+    inputItems() {
+        const items = new ItemList();
+
+        items.add('selected', m('span.TermsInput-selected', this.selectedTerms.map(term => {
+            return m('span.TermsInput-term', {
+                onclick: () => {
+                    this.toggleTerm(term);
+                    this.onready();
+                },
+            }, termLabel(term));
+        })), 20);
+
+        items.add('control', m('input.FormControl', {
+            placeholder: extractText(this.getInstruction()),
+            value: this.searchFilter,
+            oninput: event => {
+                this.searchFilter = event.target.value;
+                this.activeListIndex = 0;
+            },
+            onkeydown: this.navigator.navigate.bind(this.navigator),
+            onfocus: () => this.inputIsFocused = true,
+            onblur: () => {
+                // We delay the redraw so that clicking something doesn't immediately removes the focus
+                // This isn't necessary for the features bundled in the extension, but necessary for the
+                // Dropdown variation of this component that can be implemented by other extensions
+                m.redraw.strategy('none');
+
+                this.inputIsFocused = false;
+
+                setTimeout(() => {
+                    m.redraw();
+                }, 100);
+            },
+        }), 10);
+
+        return items;
+    }
+
+    listAvailableTerms(terms) {
+        return m('.Modal-footer', this.availableTerms === null ?
+            LoadingIndicator.component() :
+            m('ul.ChooseTaxonomyTermsModal-list.SelectTermList', terms.map(this.listAvailableTerm.bind(this)))
+        );
+    }
+
+    listAvailableTerm(term, index) {
+        return m('li.SelectTermListItem', {
+            'data-index': index,
+            className: classList({
+                colored: !!term.color(),
+                selected: this.indexInSelectedTerms(term) !== -1,
+                active: this.activeListIndex === index,
+            }),
+            style: {color: term.color()},
+            onmouseover: () => this.activeListIndex = index,
+            onclick: this.toggleTerm.bind(this, term),
+        }, [
+            taxonomyIcon(term),
+            m('span.SelectTermListItem-name', term.exists ? highlight(term.name(), this.searchFilter) : app.translator.trans('fof-taxonomies.forum.modal.custom', {
+                value: m('em', term.name()),
+            })),
+            term.description() ? m('span.SelectTermListItem-description', term.description()) : '',
+        ]);
     }
 
     toggleTerm(term) {
@@ -248,13 +297,13 @@ export default class ChooseTaxonomyTermsModal extends Modal {
     }
 
     getDomElement(index) {
-        return this.$(`li[data-index="${index}"]`);
+        return this.$(`.SelectTermListItem[data-index="${index}"]`);
     }
 
     setIndex(index, scrollToItem) {
         const $dropdown = this.$('.ChooseTaxonomyTermsModal-list');
 
-        const indexLength = this.$('.ChooseTaxonomyTermsModal-list > li').length;
+        const indexLength = this.$('.SelectTermListItem').length;
 
         if (index < 0) {
             index = indexLength - 1;
@@ -291,29 +340,7 @@ export default class ChooseTaxonomyTermsModal extends Modal {
         event.preventDefault();
 
         if (this.props.resource) {
-            this.props.resource.save({
-                relationships: {
-                    taxonomies: [
-                        {
-                            verbatim: true, // Flarum workaround, handled in addComposerControls
-                            type: 'fof-taxonomies',
-                            id: this.props.taxonomy.id(),
-                            relationships: {
-                                terms: {
-                                    data: this.selectedTerms.map(termToIdentifier),
-                                },
-                            },
-                        },
-                    ],
-                },
-            }).then(() => {
-                if (app.current instanceof DiscussionPage) {
-                    app.current.stream.update();
-                }
-                m.redraw();
-
-                app.modal.close();
-            });
+            this.saveResource();
 
             // Do not run the normal code - it might close the modal even if an error occurred
             return;
@@ -324,5 +351,41 @@ export default class ChooseTaxonomyTermsModal extends Modal {
         app.modal.close();
 
         m.redraw.strategy('none');
+    }
+
+    saveResource() {
+        this.saving = true;
+
+        this.props.resource.save({
+            relationships: {
+                taxonomies: [
+                    {
+                        verbatim: true, // Flarum workaround, handled in addComposerControls
+                        type: 'fof-taxonomies',
+                        id: this.props.taxonomy.id(),
+                        relationships: {
+                            terms: {
+                                data: this.selectedTerms.map(termToIdentifier),
+                            },
+                        },
+                    },
+                ],
+            },
+        }).then(this.onsaved.bind(this), this.onerror.bind(this));
+    }
+
+    onsaved() {
+        if (app.current instanceof DiscussionPage) {
+            app.current.stream.update();
+        }
+        this.saving = false;
+        m.redraw();
+
+        app.modal.close();
+    }
+
+    onerror() {
+        this.saving = false;
+        m.redraw();
     }
 }
